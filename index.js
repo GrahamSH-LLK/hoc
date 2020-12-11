aecdf101cac7ce2f4456f00b8d13ec758c784168
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-12-11 14:35:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,36 +3,41 @@ function quiz() {
 	 var totalQuestions = 3;
     alert("Welcome to my trivia game!");
     alert("Make sure you answer all the questions.");
-    
-     var q1 = prompt('What is 1/2 of 2?');       
-	if(q1 == "1"    ) {
-		 score = score + 1;
-		 alert("Correct!"); 
+    Swal.mixin({
+  input: 'text',
+  confirmButtonText: 'Next &rarr;',
+  showCancelButton: true,
+  progressSteps: ['1', '2', '3']
+}).queue([
+  {
+    title: 'Question 1',
+    text: 'What is 1/2 of 2?'
+  },
+  {
+    title: 'Question 2',
+    text: 'What is 1/2 of 4?'
+  },
+  {
+    title: 'Question 3',
+    text: 'What is 1/2 of 5?'
+  },
+	    
 
-	} else {
-		 alert("Incorrect!"); 
+]).then((result) => {
+  if (result.value) {
+    const answers = JSON.stringify(result.value)
+    Swal.fire({
+      title: 'All done!',
+      html: `
+        Your answers:
+        <pre><code>${answers}</code></pre>
+      `,
+      confirmButtonText: 'Lovely!'
+    })
+  }
+})
 
-	}
-    
-        var q2 = prompt('What is 1/2 of 4?');         
-	if(  q2 == "2"   ) {
-		 score = score + 1; 
-		 alert("Correct!"); 
-
-	} else {
-		 alert("Incorrect!"); 
-
-	}
-   
-      var q3 = prompt('What is 1/2 of 5?');          
-	if(  q3 == "2.5"   ) {
-		 score = score + 1; 
-		alert("Correct!");  
-
-	} else {
-		 alert("Incorrect!"); 
-
-	}
+ 
 
 	alert("You got " + score + " questions out of " + totalQuestions + " correct.");  
 }
